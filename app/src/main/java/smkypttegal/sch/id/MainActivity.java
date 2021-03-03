e4b6c217a7570f3367af34da054685f04fd42a0f
wahyudin163 <deswa.info@gmail.com>
2021-03-03 15:01:09 +0700

    Fitur Tombol back

--- app/src/main/java/smkypttegal/sch/id/MainActivity.java
+++ app/src/main/java/smkypttegal/sch/id/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.Window;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 //    WebView webviewku;
@@ -35,6 +36,18 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean shouldOverrideKeyEvent(WebView view, KeyEvent event) {
             return false;
+        }
+    }
+
+//    menu kembali berfungsi
+    @Override
+    public void onBackPressed(){
+        if (web.canGoBack()) {
+            web.goBack();
+            Toast.makeText(this, "Balik maning nang tampilan awal", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this, "Exiting a webview", Toast.LENGTH_SHORT).show();
+            super.onBackPressed();
         }
     }
 }
